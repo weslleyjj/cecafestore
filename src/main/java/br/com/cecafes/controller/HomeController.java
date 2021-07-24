b851ce4824b58b9f7dc1ce40b8073701ee4792c3
Jodeilson Weslley <jodeilson.weslley@logiquesistemas.com.br>
2021-07-23 23:38:24 -0300

    Montagem das views para visualizar cada página do template

--- src/main/java/br/com/cecafes/controller/HomeController.java
+++ src/main/java/br/com/cecafes/controller/HomeController.java
@@ -11,4 +11,45 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @RequestMapping(value = "/shop", method = RequestMethod.GET)
+    public String shop() {
+        return "shop-details";
+    }
+
+    @RequestMapping(value = "/shop-grid", method = RequestMethod.GET)
+    public String shopGrid() {
+        return "shop-grid";
+    }
+
+    @RequestMapping(value = "/shop-cart", method = RequestMethod.GET)
+    public String shopCart() {
+        return "shoping-cart";
+    }
+
+    @RequestMapping(value = "/main", method = RequestMethod.GET)
+    public String main() {
+        return "main";
+    }
+
+    @RequestMapping(value = "/contact", method = RequestMethod.GET)
+    public String contact() {
+        return "contact";
+    }
+
+    @RequestMapping(value = "/checkout", method = RequestMethod.GET)
+    public String checkout() {
+        return "checkout";
+    }
+
+    @RequestMapping(value = "/blog-details", method = RequestMethod.GET)
+    public String blogDetails() {
+        return "blog-details";
+    }
+
+    @RequestMapping(value = "/blog", method = RequestMethod.GET)
+    public String blog() {
+        return "blog";
+    }
+
 }
