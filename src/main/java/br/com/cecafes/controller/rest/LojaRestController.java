1069c2371df32803b20bc37abf0f9db5023e7c08
Jodeilson Weslley <jodeilson.weslley@logiquesistemas.com.br>
2021-08-24 23:22:01 -0300

    Feito rota de remoção de produto no cookie

--- src/main/java/br/com/cecafes/controller/rest/LojaRestController.java
+++ src/main/java/br/com/cecafes/controller/rest/LojaRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @RestController(value = "/loja")
 public class LojaRestController {
@@ -44,5 +45,42 @@ public class LojaRestController {
 
 
         return ResponseEntity.ok("produto adicionado");
+    }
+
+    @GetMapping("/remove-item/{id}")
+    public ResponseEntity<?> removerItemCookie(@PathVariable(name = "id") Long id, HttpServletResponse response, HttpServletRequest request){
+        Cookie[] cookiesRequest = request.getCookies();
+
+        try{
+            if (CookieUtil.verificaCookies(cookiesRequest, "produtos")){
+                Cookie cookie = CookieUtil.procurarCookie(cookiesRequest, "produtos");
+
+                String valorCookie = cookie.getValue();
+
+                if(valorCookie.contains(id.toString())){
+                    String[] ids = valorCookie.split("/");
+                    StringBuilder novoCookie = new StringBuilder();
+
+                    for (String s : ids) {
+                        if(!s.equals(id.toString())){
+                           novoCookie.append(s).append("/");
+                        }
+                    }
+
+                    cookie.setValue(novoCookie.toString());
+                    cookie.setMaxAge(60 * 60 * 24 * 30);
+                    cookie.setPath("/");
+                    response.addCookie(cookie);
+                }
+
+            }else{
+                return ResponseEntity.badRequest().build();
+            }
+        }catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
+
+
+        return ResponseEntity.ok("produto removido");
     }
 }
