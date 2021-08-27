f965c04e3cb8791d393ede11be0c75d89bc1f801
Gustavo <gu-gu-pf@hotmail.com>
2021-08-27 00:02:18 -0300

    criação da loja de produtos
    foi criada a loja, realizando a paginação e a busca de todos os produtos, finalizando também os tratamentos dos links. Está pendente a lógica de produto recentes e produtos em promoção

--- src/main/java/br/com/cecafes/controller/LojaController.java
+++ src/main/java/br/com/cecafes/controller/LojaController.java
@@ -1,15 +1,72 @@
 package br.com.cecafes.controller;
 
+import br.com.cecafes.model.Comprador;
+import br.com.cecafes.model.ProdutoCecafes;
+import br.com.cecafes.model.User;
+import br.com.cecafes.repository.UserRepository;
+import br.com.cecafes.security.MyUserDetails;
+import br.com.cecafes.service.CompradorService;
+import br.com.cecafes.service.ProdutoCecafesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping(value = "/loja")
 public class LojaController {
+    private ProdutoCecafesService produtoCecafesService;
+    private CompradorService compradorService;
+    private UserRepository userRepository;
+
+    @Autowired
+    public LojaController(
+            ProdutoCecafesService produtoCecafesService,
+            CompradorService compradorService,
+            UserRepository userRepository
+    ) {
+        this.produtoCecafesService = produtoCecafesService;
+        this.compradorService = compradorService;
+        this.userRepository = userRepository;
+    }
 
     @GetMapping
-    public String homeLoja() {
+    public String homeLoja(
+            Model model,
+            @RequestParam("page") Optional<Integer> page,
+            @RequestParam("size") Optional<Integer> size
+    ) {
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(9);
+
+        Page<ProdutoCecafes> produtoPage;
+
+        List<ProdutoCecafes> produtos = produtoCecafesService.findAll();
+        produtoPage = produtoCecafesService.findPaginated(PageRequest.of(currentPage - 1, pageSize));
+
+        model.addAttribute("produtos", produtos);
+        model.addAttribute("produtoPage", produtoPage);
+        int totalPages = produtoPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
         return "shop-grid";
     }
 }
--- src/main/java/br/com/cecafes/service/ProdutoCecafesService.java
+++ src/main/java/br/com/cecafes/service/ProdutoCecafesService.java
@@ -1,10 +1,17 @@
 package br.com.cecafes.service;
 
+import br.com.cecafes.model.Pedido;
 import br.com.cecafes.model.ProdutoCecafes;
 import br.com.cecafes.repository.ProdutoCecafesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,5 +42,25 @@ public class ProdutoCecafesService {
 
     public void deleteById(Long id) {
         repository.deleteById(id);
+    }
+
+    public Page<ProdutoCecafes> findPaginated(Pageable pageable) {
+        List<ProdutoCecafes> produtos = repository.findAll();
+
+        int pageSize = pageable.getPageSize();
+        int currentPage = pageable.getPageNumber();
+        int startItem = currentPage * pageSize;
+        List<ProdutoCecafes> list;
+
+        if (produtos.size() < startItem) {
+            list = Collections.emptyList();
+        } else {
+            int toIndex = Math.min(startItem + pageSize, produtos.size());
+            list = produtos.subList(startItem, toIndex);
+        }
+
+        Page<ProdutoCecafes> pedidoPage = new PageImpl<>(list, PageRequest.of(currentPage, pageSize), produtos.size());
+
+        return pedidoPage;
     }
 }
