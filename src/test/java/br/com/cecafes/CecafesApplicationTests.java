1db23d48a0f00d575a1f6710d74481e4168915b0
Jodeilson Weslley <jodeilson.weslley@logiquesistemas.com.br>
2021-09-07 16:36:46 -0300

    ajustes na classe para testes unitários

--- src/test/java/br/com/cecafes/CecafesApplicationTests.java
+++ src/test/java/br/com/cecafes/CecafesApplicationTests.java
@@ -3,11 +3,12 @@ package br.com.cecafes;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CecafesApplicationTests.class)
 class CecafesApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("Testes unitários ainda não implementados");
     }
 
 }
